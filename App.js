01dfdb33ebc066b0cc55598d9746a9ca9988275e
sterf-mac <stefhwin@gmail.com>
2024-04-28 21:39:54 -0400

    layout and colors organized

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
   const [bannerText, setBannerText] = useState('');
+  const [token, setToken] = useState(null);
+
   const onPressDoorbotBuzz = () => {
     setBannerText('Buzz button pressed');
+    console.log('buzz pressed');
   }
-    
+  
   const onPressDoorbotElevator = () => {
     setBannerText('Elevator button pressed'); 
+    console.log('elevator pressed');
   }
   
   const onPressDoorbotStatus = () => {
     setBannerText('Status button pressed');
+    console.log('status pressed');
+  }
+
+  const onPressSettings = () => {
+    setBannerText('Settings button pressed');
+    console.log('settings pressed');
   }
 
   return (
@@ -21,12 +31,12 @@ export default function App() {
       <View style={styles.banner} >
         <Text style={styles.bannerText}>{bannerText}</Text>
       </View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Does this update real time?</Text>
-      <Button title="BUZZ" onPress={() => onPressDoorbotBuzz()}/>
-      <Button title="ELEVATOR" onPress={() => onPressDoorbotElevator()}/>
-      <Button title="CHECK STATUS" onPress={() => onPressDoorbotStatus()}/>
-      {/* <StatusBar style="auto" /> */}
+      <TouchableOpacity style={[styles.button, {backgroundColor: '#529ce6'}]} onPress={() => onPressDoorbotBuzz()}><Text style={styles.buttonText}>BUZZ</Text></TouchableOpacity>
+      <TouchableOpacity style={[styles.button, {backgroundColor: '#ff6a73'}]}  onPress={() => onPressDoorbotElevator()}><Text style={styles.buttonText}>ELEVATOR</Text></TouchableOpacity>
+      <TouchableOpacity style={[styles.button, {backgroundColor: '#d55ad5'}]}  onPress={() => onPressDoorbotStatus()}><Text style={styles.buttonText}>CHECK STATUS</Text></TouchableOpacity>
+      <View style={styles.settings}>
+        <Button title="SETTINGS" onPress={() => onPressSettings()}/>
+      </View>
     </View>
   );
 }
@@ -41,23 +51,38 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   banner: {
-    backgroundColor: 'red',
-    padding: 10,
-    width: '100%',
+    position: 'absolute',
+    top: 50,
+    height: 60,
+    backgroundColor: '#73bdff',
+    width: '110%',
     alignItems: 'center',
+    justifyContent: 'center',
   },
   bannerText: {
     color: 'white',
+    fontSize: 20,
   },
+  button: {
+    height: 120,
+    width: 250,
+    backgroundColor: 'lavender',
+    marginTop: 15,
+    marginBottom: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  settings: {
+    position: 'absolute',
+    right: 25,
+    bottom: 25,
+  }
 });
 
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flexDirection: 'column',
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
